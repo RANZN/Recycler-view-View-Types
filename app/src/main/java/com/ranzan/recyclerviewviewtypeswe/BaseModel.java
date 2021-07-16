f8a5bc2d3d51e0caa645b7c207d455af97a8bfa8
PRAKASH RANJAN <aryaprakashranjan@gmail.com>
2021-07-16 23:59:41 +0530

    Initial commit

---
+++ app/src/main/java/com/ranzan/recyclerviewviewtypeswe/BaseModel.java
@@ -0,0 +1,5 @@
+package com.ranzan.recyclerviewviewtypeswe;
+
+public abstract class BaseModel {
+    abstract int getViewType();
+}
---
+++ app/src/main/java/com/ranzan/recyclerviewviewtypeswe/ReceiverViewHolder.java
@@ -0,0 +1,26 @@
+package com.ranzan.recyclerviewviewtypeswe;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReceiverViewHolder extends RecyclerView.ViewHolder {
+    private TextView mTvReceiverSendMessage;
+
+    public ReceiverViewHolder(@NonNull View itemView) {
+        super(itemView);
+        initViews(itemView);
+
+    }
+
+    private void initViews(View itemView) {
+        mTvReceiverSendMessage = itemView.findViewById(R.id.tvReceiverMessage);
+    }
+
+    public void setData(ReceiverModel receiverModel) {
+        mTvReceiverSendMessage.setText(receiverModel.getReceiverMessage());
+
+    }
+}
